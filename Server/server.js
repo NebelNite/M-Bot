d830962769c5df4b2144d82e43552815f15c8035
bogen <bogensbergerjonas12345@gmail.com>
2024-02-09 23:08:08 +0100

    Password-fixed

--- Server/server.js
+++ Server/server.js
@@ -1,33 +1,37 @@
 //npm run start2
+
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 const session = require('express-session');
 
 
+
+
 const app = express();
 const port = 3000; 
-const ip = '10.10.0.172';
+// ip = '10.10.0.172';
+const ip = '192.168.0.18';
 
-const password = process.env.PASSWORD_HASH;
+
+const fixPassword = process.env.PASSWORD_HASH;
+
+app.use(session({
+    secret: 'mysecret',
+    resave: false,
+    saveUninitialized: true
+}));
 
 
 // Middleware für das Parsen von JSON-Daten
 app.use(bodyParser.json());
-
-// Endpunkt für die Befehlsverarbeitung
-app.post('/befehl', (req, res) => {
-    const command = req.body.command;
-    console.log('Empfangener Befehl:', command);
-
-    // Hier kannst du den Befehl an den mBot senden
-
-    res.send('Befehl empfangen und verarbeitet');
-});
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
 
-app.get('/movement',requireLogin, (req, res) => {
+
+app.get('/movement', requireLogin, (req, res) => {
 
     res.sendFile(process.cwd()+"/html/movement.html");
 
@@ -37,47 +41,65 @@ app.get('/movement',requireLogin, (req, res) => {
 });
 
 // Endpunkt für die Bewegungssteuerung
-app.post('/movement', (req, res) => {
-    const command = req.body.command;
-    console.log('Empfangener Befehl:', command);
+    app.post('/movement', (req, res) => {
+        if (req.session && req.session.loggedIn) {
+            const command = req.body.command;
+            console.log('Empfangener Befehl:', command);
 
-    
-    // Hier kannst du den Befehl an den mBot senden
+            // Hier kannst du den Befehl an den mBot senden
+            res.send('Command: ' + command);
+            
+        } else {
+            // Benutzer nicht angemeldet, leite ihn zur Login-Seite weiter
+            res.redirect("http://" + ip + ":" + port + "/login");
+        }
+    });
 
-    res.send('Command: ', command);
-});
 
-
-app.use(session({
-    secret: 'mysecret', // Geheimnis zur Signierung der Sitzungs-Cookies
-    resave: false,
-    saveUninitialized: true
-}));
 
 function requireLogin(req, res, next) {
     if (req.session && req.session.loggedIn) {
         return next(); // Erlaube den Zugriff auf die Seite, wenn der Benutzer angemeldet ist
     } else {
-        //res.redirect(process.cwd()+"/html/login.html"); // Benutzer zur Login-Seite umleiten, wenn nicht angemeldet
+        res.redirect("http://" + ip + ":" + port + "/login");
     }
 }
+
 // Route für die Login-Seite
 app.get('/login', (req, res) => {
     res.sendFile(process.cwd()+"/html/login.html");
 });
 
+
 function checkPassword(req, res, next) {
+    
     const enteredPassword = req.body.password;
-    // Vergleiche das eingegebene Passwort mit dem gespeicherten, nachdem es entschlüsselt wurde
-    bcrypt.compare(enteredPassword, password, (err, result) => {
-        if (err || !result) {
-            res.status(401).send('Ungültiges Passwort!');
-        } else {
-            next(); // Erlaube den Zugriff auf die Movement-Seite
-        }
-    });
+
+    // Überprüfe, ob das Passwort vorhanden und nicht leer ist
+    if (!enteredPassword) {
+        return res.status(400).send('Passwort fehlt');
+    }
+
+
+        bcrypt.compare(enteredPassword, fixPassword, (err, result) => {
+            if (err || !result) {
+                return res.status(401).send('Ungültiges Passwort!');
+            } else {
+                // Wenn das Passwort korrekt ist, erlaube den Zugriff auf die Movement-Seite
+                req.session.loggedIn = true;
+                res.redirect("http://" + ip + ":" + port + "/movement");
+            }
+        });
 }
 
+
+
+
+
+app.post('/login', checkPassword, (req, res) => {
+    // Wenn das Passwort korrekt ist, wird die Benutzersitzung erstellt und zur Movement-Seite weitergeleitet
+
+});
 
 
 
